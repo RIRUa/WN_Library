b6c8f64b80ee5edc838952ac737db6e9685c37c2
RIRUa <is0531si@ed.ritsumei.ac.jp>
2021-02-20 21:41:35 +0900

    first commit!!

---
+++ WN_Library/WN_Library/WN_Library.h
@@ -0,0 +1,18 @@
+//
+//  WN_Library.h
+//  WN_Library
+//
+//  Created by 渡辺奈央騎 on 2021/02/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WN_Library.
+FOUNDATION_EXPORT double WN_LibraryVersionNumber;
+
+//! Project version string for WN_Library.
+FOUNDATION_EXPORT const unsigned char WN_LibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WN_Library/PublicHeader.h>
+
+
